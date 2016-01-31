4717ea052708c109f861ef4f9c065f34c357132a
nakaded <nakaded1018@gamail.com>
2016-01-31 15:38:22 +0900

    ddddddd

--- js/touch.js
+++ js/touch.js
@@ -1,5 +1,24 @@
 $(function(){
 
+
+ //緯度・経度
+ var lon;
+ var lat;
+
+    //GPS
+    navigator.geolocation.getCurrentPosition(function(evt){
+    //緯度
+    var lon  = evt.coords.longitude;
+    //経度
+    var lat  = evt.coords.latitude;
+    },
+    function(error){
+        console.log("GPS取れなかったよー");
+    });
+
+
+
+
     $("#ball").on("touchstart",function(){
         $("#touchResult li").eq(0).text("touchstart したよ");
     });
@@ -10,6 +29,9 @@ $(function(){
 
     $("#ball").on("touchend",function(){
         $("#touchResult li").eq(2).text("touchend したよ");
+
+        $("#geo li").eq(0).text("緯度: "+ lon);
+        $("#geo li").eq(1).text("経度: "+ lat);
     });
 
 });
