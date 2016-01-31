d1f8921b050ebe0290bc690b2d62341721e55875
nakaded <nakaded1018@gamail.com>
2016-01-31 14:47:15 +0900

    devicemotion

--- js/deviceMotion.js
+++ js/deviceMotion.js
@@ -1,17 +1,16 @@
 $(function(){
 
     //加速度のイベント
-    window.addEventListener("devicemotion",shakeDevice);
+    window.addEventListener('devicemotion',shakeDevice);
 
     function shakeDevice(evt){
       var x = evt.acceleration.x;
       var y = evt.acceleration.y;
       var z = evt.acceleration.Z;
 
-      $("#device li").eq(0).text("加速度X: "+x);
-      $("#device li").eq(1).text("加速度Y: "+y);
-      $("#device li").eq(2).text("加速度Z: "+z);
-
+      $('#device li').eq(0).text('加速度X: ' + x);
+      $('#device li').eq(1).text('加速度Y: ' + y);
+      $('#device li').eq(2).text('加速度Z: ' + z);
     }
 
 });
