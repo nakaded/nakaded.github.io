09e2783cacce2057bb0490fee2c5cbdfc1e28870
nakaded <nakaded1018@gamail.com>
2016-02-07 16:19:27 +0900

    aaaaaaaaa

--- createjs/js/splite.js
+++ createjs/js/splite.js
@@ -36,7 +36,7 @@ function showVege(){
 	vitaminC.y = 400;
 	vitaminC.alpha = 0;
 
-	creatjs.Tween.get(vitaminC).to({alpha:1},500).walk(1000).to({x:500},400);
+	createjs.Tween.get(vitaminC).to({alpha:1},500).walk(1000).to({x:500},400);
 
 	stage.update();
 };
