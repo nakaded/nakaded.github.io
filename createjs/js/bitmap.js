d0067bfa0def5ae2ab71948f831bed57c4fc0d63
nakaded <nakaded1018@gamail.com>
2016-02-07 15:13:19 +0900

    bitmap

---
+++ createjs/js/bitmap.js
@@ -0,0 +1,38 @@
+$(function(){
+
+	// stage
+	var stage = new createjs.Stage("canvasEl");
+
+  // 画像を保存する配列
+  var manifest = [
+    {src:'img/tomato.png'}
+	{src:"img/pumpkin.png"}
+	{src:"img/greenpeppar.png"}
+  ];
+
+  // loadQueueクラス
+  var loadQueue = new createjs.LoadQueue();
+
+  // 同時に何ファイル読み込むか
+  // loadQueue.setMaxConnections(6);
+
+  // 読み込み開始
+  loadQueue.loadManifest(manifest);
+
+
+  // 全部読み込み終わったら
+  loadQueue.addEventListener("complete", function(evt){
+    console.log('読み込み完了');
+    addStage();
+  });
+
+
+
+	createjs.Ticker.timingMode = createjs.Ticker.RAF_SYNCHED;
+	createjs.Ticker.setFPS(30);
+
+  createjs.Ticker.addEventListener('tick',function(){
+    stage.update();
+  })
+
+});
